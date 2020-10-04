9320bbfcc3faba6a7b1964526beffe393a7b2a94
Sumit Govil <sumit.govil@optus.com.au>
2020-10-04 22:41:36 +1100

    🍕 toppings filter

---
+++ src/components/ToppingsFilter.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { useStaticQuery, graphql, Link } from 'gatsby';
+import styled from 'styled-components';
+
+const ToppingsStyles = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  margin-bottom: 4rem;
+  a {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-gap: 0 1rem;
+    align-items: center;
+    padding: 5px;
+    background: var(--grey);
+    border-radius: 2px;
+    .count {
+      background: white;
+      padding: 2px 5px;
+    }
+    .active {
+      background: var(--yellow);
+    }
+  }
+`;
+
+const allToppingsQuery = graphql`
+  query {
+    pizzas: allSanityPizza {
+      nodes {
+        toppings {
+          name
+          id
+        }
+      }
+    }
+  }
+`;
+
+function countPizzasInToppings(pizzas) {
+  const counts = pizzas
+    .map((pizza) => pizza.toppings)
+    .flat()
+    .reduce((acc, topping) => {
+      const existingTopping = acc[topping.name];
+      if (existingTopping) {
+        existingTopping.count += 1;
+      } else {
+        acc[topping.name] = {
+          id: topping.id,
+          name: topping.name,
+          count: 1,
+        };
+      }
+      return acc;
+    }, {});
+
+  const sortedToppings = Object.values(counts).sort(
+    (a, b) => b.count - a.count
+  );
+  return sortedToppings;
+}
+
+const ToppingsFilter = () => {
+  const { pizzas } = useStaticQuery(allToppingsQuery);
+  const toppingsWithCounts = countPizzasInToppings(pizzas.nodes);
+
+  return (
+    <ToppingsStyles>
+      {toppingsWithCounts.map((topping) => (
+        <Link to={`/topping/${topping.name}`} key={topping.id}>
+          <span className="name">{topping.name}</span>
+          <span className="count">{topping.count}</span>
+        </Link>
+      ))}
+    </ToppingsStyles>
+  );
+};
+
+export default ToppingsFilter;
